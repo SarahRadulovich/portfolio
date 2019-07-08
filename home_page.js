31756fe085033a56e773708129bcd8cbbcf33192
Sarah Radulovich <sarah.j.radulovich@gmail.com>
2019-07-08 18:14:40 -0400

    Style changes

--- home_page.js
+++ home_page.js
@@ -1,1 +1,20 @@
 //javascript file
+
+//used for actual web functionality, buttons, etc.
+$(document).ready(function() {
+    /* Navigation smooth scroll */
+    $(function() {
+      $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html, body').animate({
+              scrollTop: target.offset().top-30
+            }, 1000);
+            return false;
+          }
+        }
+      });
+    });
+});
